9faf0e0cae7e0eaf1aa5cb0aa6b592b0ea7bc11f
wizock <silent.death3500@gmail.com>
2023-06-30 01:19:47 +1000

    zstd

--- nuclei_backend/storage_service/image_compression/image_compression_utils.py
+++ nuclei_backend/storage_service/image_compression/image_compression_utils.py
@@ -93,23 +93,12 @@ class CompressImage(CompressionImpl):
 
     def produce_compression(self) -> bytes:
         print("compressing image")
-        image = PIL.Image.open(self.compression_temp_file[0], "r")
-        # make sure the image is in RGB mode
-        if image.mode != "RGB":
-            image = image.convert("RGB")
-        # assure the image is in the correct orientation
-        # image = image.transpose(PIL.Image.ROTATE_270)
-        print("image size ", image.size)
-        image.save(
-            self.compression_temp_file[0],
-            format="JPEG",
-            optimize=True,
-            progressive=True,
-            quality=85,
-        )
-        print("image compressed2x")
         with open(self.compression_temp_file[0], "rb") as f:
-            print("image compressed3x")
-            compressed_file = f.read()
+            original_data = f.read()
 
-        return compressed_file
+        try:
+            compressor = zstd.ZstdCompressor(level=15)
+            return compressor.compress(original_data)
+        except Exception as e:
+            print(f"Error compressing image: {str(e)}")
+            return b""
