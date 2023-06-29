b2b5f9020fe0f89488e644c2883c6fc36ee4da51
wizock <silent.death3500@gmail.com>
2023-06-30 00:57:13 +1000

    .

--- nuclei_backend/syncing_service/sync_user_cache.py
+++ nuclei_backend/syncing_service/sync_user_cache.py
@@ -12,7 +12,7 @@ import base64
 class RedisController:
     def __init__(self, user):
         self.redis_connection = redis.Redis().from_url(
-            url="redis://redis:6379", decode_responses=True, db=0
+            url="redis://127.0.0.1:6379", decode_responses=True, db=0
         )
         self.user = user
 
@@ -52,7 +52,7 @@ class FileCacheEntry:
         """Create a new file cache entry for the specified directory."""
         self.dir_id = dir_id
         self.redis_connection = redis.Redis().from_url(
-            url="redis://redis:6379", decode_responses=True, db=1
+            url="redis://127.0.0.1:6379", decode_responses=True, db=1
         )
 
     def activate_file_session(self):
