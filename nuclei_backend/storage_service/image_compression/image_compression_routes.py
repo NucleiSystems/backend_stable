962b2d9bb8339e51233f58a4b2f8ac4e0c8fb4c2
wizock <silent.death3500@gmail.com>
2023-06-30 01:15:58 +1000

    .

--- nuclei_backend/storage_service/image_compression/image_compression_routes.py
+++ nuclei_backend/storage_service/image_compression/image_compression_routes.py
@@ -11,73 +11,101 @@ from typing import List
 import io
 
 
-def process_file(
-    file: bytes, filename: str, ipfs_flag: bool, identity_token: str, db
-) -> None:
-    try:
-        compressing_file = CompressImage(file, filename)
+# def process_file(
+#     file: bytes, filename: str, ipfs_flag: bool, identity_token: str, db
+# ) -> None:
+#     try:
+#         compressing_file = CompressImage(file, filename)
 
-        print("files compressed")
-        compressed_file = compressing_file.produce_compression()
-        print(compressed_file)
-        if ipfs_flag:
-            print("before ipfs flag")
-            try:
-                compressing_file.commit_to_ipfs(
-                    compressed_file, filename, identity_token, db
-                )
-            except Exception as e:
-                print(f"the error was {e}")
-        compressing_file.cleanup_compression_outcome()
-    except Exception as e:
-        print(f"Error compressing and storing file {filename}: {str(e)}")
+#         print("files compressed")
+#         compressed_file = compressing_file.produce_compression()
+#         print(compressed_file)
+#         if ipfs_flag:
+#             print("before ipfs flag")
+#             try:
+#                 compressing_file.commit_to_ipfs(
+#                     compressed_file, filename, identity_token, db
+#                 )
+#             except Exception as e:
+#                 print(f"the error was {e}")
+#         compressing_file.cleanup_compression_outcome()
+#     except Exception as e:
+#         print(f"Error compressing and storing file {filename}: {str(e)}")
 
 
-def process_files(
+# def process_files(
+#     files: List[UploadFile],
+#     ipfs_flag: bool | None = True,
+#     identity_token: str = Depends(get_current_user),
+#     db=Depends(get_db),
+# ):
+#     with ThreadPoolExecutor(max_workers=8) as executor:
+#         futures = []
+#         for file in files:
+#             _filename = file.filename.replace(" ", "_")
+#             _file = file.file.read()  # Read the file contents as bytes
+#             future = executor.submit(
+#                 process_file, _file, _filename, ipfs_flag, identity_token, db
+#             )
+#             futures.append(future)
+
+#         results = [future.result() for future in futures]
+
+#     return results
+
+
+# @storage_service.post("/compress/image")
+# async def compress_task_image(
+#     files: List[UploadFile],  # noqa: F405
+#     background_tasks: BackgroundTasks,
+#     ipfs_flag: bool | None = True,
+#     identity_token: str = Depends(get_current_user),
+#     db=Depends(get_db),
+# ):
+#     # print the files
+#     for file in files:
+#         print(file.filename)
+#         print(file.file)
+
+#     if not files:
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail="No file uploaded",
+#         )
+#     try:
+#         background_tasks.add_task(process_files, files, ipfs_flag, identity_token, db)
+
+#         return {
+#             "message": "Files submitted for compression",
+#             "files": [file.filename for file in files],
+#         }, status.HTTP_202_ACCEPTED
+#     except Exception as e:
+#         return {"error": e}
+
+
+@storage_service.post("/compress/image")
+async def compress_task_image(
     files: List[UploadFile],
     ipfs_flag: bool | None = True,
     identity_token: str = Depends(get_current_user),
     db=Depends(get_db),
 ):
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = []
-        for file in files:
-            _filename = file.filename.replace(" ", "_")
-            _file = file.file.read()  # Read the file contents as bytes
-            future = executor.submit(
-                process_file, _file, _filename, ipfs_flag, identity_token, db
-            )
-            futures.append(future)
-
-        results = [future.result() for future in futures]
-
-    return results
-
-
-@storage_service.post("/compress/image")
-async def compress_task_image(
-    files: List[UploadFile],  # noqa: F405
-    background_tasks: BackgroundTasks,
-    ipfs_flag: bool | None = True,
-    identity_token: str = Depends(get_current_user),
-    db=Depends(get_db),
-):
-    # print the files
     for file in files:
-        print(file.filename)
-        print(file.file)
+        _file = file.file
+        _file = _file.read()
+        _filename = file.filename.replace(" ", "_")
 
-    if not files:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="No file uploaded",
-        )
-    try:
-        background_tasks.add_task(process_files, files, ipfs_flag, identity_token, db)
+        if identity_token is None:
+            return {"message": "Unauthorized user"}, status.HTTP_401_UNAUTHORIZED
+        if not file:
+            return {"error": "No file uploaded"}
 
-        return {
-            "message": "Files submitted for compression",
-            "files": [file.filename for file in files],
-        }, status.HTTP_202_ACCEPTED
-    except Exception as e:
-        return {"error": e}
+        compressing_file = CompressImage(_file, _filename)
+
+        compressed_file = compressing_file.produce_compression()
+        if ipfs_flag:
+            compressing_file.commit_to_ipfs(
+                compressed_file, _filename, identity_token, db
+            )
+        compressing_file.cleanup_compression_outcome()
+    return {"message": "Successfully compressed video"}, status.HTTP_200_OK
--- nuclei_backend/storage_service/image_compression/image_compression_utils.py
+++ nuclei_backend/storage_service/image_compression/image_compression_utils.py
@@ -4,8 +4,80 @@ import pathlib
 import psutil
 import zstandard as zstd
 import numpy as np
+import logging
+import pathlib
+import subprocess
+from typing import Final
+from uuid import uuid4
+from functools import lru_cache
+
+import PIL.Image
+from fastapi import File
 
 from ..CompressionBase import CompressionImpl
+
+logger = logging.getLogger(__name__)
+from ..CompressionBase import CompressionImpl
+
+
+# class CompressImage(CompressionImpl):
+#     def __init__(self, file: bytes, filename: str):
+#         super().__init__(app_path="image")
+
+#         self.file = file
+#         self.filename = filename
+#         self.compression_temp_file = self.save_to_temp(self.file, self.filename)
+
+#     def cleanup_compression_outcome(self):
+#         pathlib.Path(self.compression_temp_file[0]).unlink()
+
+#     def compress_data(self, data: bytes):
+#         try:
+#             cpu_percent = psutil.cpu_percent()
+#             mem_percent = psutil.virtual_memory().percent
+
+#             if cpu_percent >= 40 and mem_percent >= 40:
+#                 file_size_mb = len(data) / 1024 / 1024
+
+#                 level = 15 if self.compression_level == 1 and file_size_mb > 300 else 1
+#             elif mem_percent < 60:
+#                 level = 20
+#             else:
+#                 level = 22
+#             compressor = zstd.ZstdCompressor(level=level)
+#             return compressor.compress(data)
+#         except Exception as e:
+#             return e
+
+#     def produce_compression(self) -> bytes:
+#         try:
+#             with open(self.compression_temp_file[0], "rb") as f:
+#                 original_data = np.fromfile(f, dtype=np.uint8)
+
+#             file_size = len(original_data)
+#             avg_image_size = 5000000  # example average size of an image in bytes
+#             if file_size < avg_image_size * 0.8:
+#                 compression_processes = 1
+#                 chunks = [original_data]
+#             else:
+#                 compression_processes = os.cpu_count()
+#                 chunk_size = (file_size // compression_processes) + 1
+#                 chunks = np.array_split(original_data, compression_processes)
+
+#             with ProcessPoolExecutor(max_workers=compression_processes) as executor:
+#                 compressed_chunks = executor.map(self.compress_data, chunks)
+
+#             compressed_data = b"".join(
+#                 chunk for chunk in compressed_chunks if isinstance(chunk, bytes)
+#             )
+
+#             return (
+#                 compressed_data.encode("utf-8")
+#                 if isinstance(compressed_data, bytes)
+#                 else None
+#             )
+#         except Exception as e:
+#             print(e)
 
 
 class CompressImage(CompressionImpl):
@@ -19,46 +91,25 @@ class CompressImage(CompressionImpl):
     def cleanup_compression_outcome(self):
         pathlib.Path(self.compression_temp_file[0]).unlink()
 
-    def compress_data(self, data: bytes):
-        try:
-            cpu_percent = psutil.cpu_percent()
-            mem_percent = psutil.virtual_memory().percent
-
-            if cpu_percent >= 40 and mem_percent >= 40:
-                file_size_mb = len(data) / 1024 / 1024
-
-                level = 15 if self.compression_level == 1 and file_size_mb > 300 else 1
-            elif mem_percent < 60:
-                level = 20
-            else:
-                level = 22
-            compressor = zstd.ZstdCompressor(level=level)
-            return compressor.compress(data)
-        except Exception as e:
-            return e
-
     def produce_compression(self) -> bytes:
-        try:
-            with open(self.compression_temp_file[0], "rb") as f:
-                original_data = np.fromfile(f, dtype=np.uint8)
+        print("compressing image")
+        image = PIL.Image.open(self.compression_temp_file[0], "r")
+        # make sure the image is in RGB mode
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        # assure the image is in the correct orientation
+        # image = image.transpose(PIL.Image.ROTATE_270)
+        print("image size ", image.size)
+        image.save(
+            self.compression_temp_file[0],
+            format="JPEG",
+            optimize=True,
+            progressive=True,
+            quality=85,
+        )
+        print("image compressed2x")
+        with open(self.compression_temp_file[0], "rb") as f:
+            print("image compressed3x")
+            compressed_file = f.read()
 
-            file_size = len(original_data)
-            avg_image_size = 5000000  # example average size of an image in bytes
-            if file_size < avg_image_size * 0.8:
-                compression_processes = 1
-                chunks = [original_data]
-            else:
-                compression_processes = os.cpu_count()
-                chunk_size = (file_size // compression_processes) + 1
-                chunks = np.array_split(original_data, compression_processes)
-
-            with ProcessPoolExecutor(max_workers=compression_processes) as executor:
-                compressed_chunks = executor.map(self.compress_data, chunks)
-
-            compressed_data = b"".join(
-                chunk for chunk in compressed_chunks if isinstance(chunk, bytes)
-            )
-
-            return compressed_data if isinstance(compressed_data, bytes) else None
-        except Exception as e:
-            print(e)
+        return compressed_file
