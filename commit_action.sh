cbd7814841479a252ce358c09796d5e8f0c33298
Wizock <silent.death3500@gmail.com>
2023-08-16 19:38:06 +1000

    simplified

---
+++ commit_action.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define your VPS SSH details
+VPS_USER="root"
+VPS_HOST="170.64.180.130"
+VPS_SSH_KEY_PATH="./id_rsa"
+
+# Define the service you want to restart
+SERVICE_NAME="nuclei_backend.service"
+
+# SSH into the VPS and restart the service
+ssh -i "$VPS_SSH_KEY_PATH" "$VPS_USER@$VPS_HOST" "sudo systemctl restart $SERVICE_NAME"
