41b2d08b172b804269981c18e6eac0ece6fa1405
crushr3sist <silent.death3500@gmail.com>
2023-12-17 23:14:53 +1000

    Remove unused import statement

--- nuclei_backend/storage_service/main.py
+++ nuclei_backend/storage_service/main.py
@@ -4,4 +4,3 @@ storage_service = APIRouter(prefix="/storage")
 
 from .image_compression.image_compression_routes import *  # noqa: E402, F403
 from .ipfs_routes import *  # noqa: E402, F403
-from .misc_compression.misc_compression_routes import *  # noqa: E402, F403
